24be5a15b855ad65768b3878309396dc01f61275
Enzo Belmonte <ebelmonte@frba.utn.edu.ar>
2022-06-06 23:48:49 -0300

    Agrego EJ4.14

---
+++ EJ4/EJ4.14/funciones.h
@@ -0,0 +1,21 @@
+/*!
+ * @file    funciones.h
+ * @brief   Implementar una función que reciba un número entero pasado por referencia y muestre cómo se almacena en memoria.
+ * @author  Enzo Belmonte <ebelmonte@frba.utn.edu.ar>
+ * @date    06/06/2022
+ */
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+
+/*!
+ * @brief Funcion que muestra cómo se almacena un entero en memoria.
+ *
+ * @param[in] numero: Puntero de entrada.
+ * @return	  -
+ */
+void mostrar_entero_en_memoria(int*);
+
+
+
+#endif // FUNCIONES_H_
